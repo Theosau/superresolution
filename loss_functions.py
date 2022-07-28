cd1ef3e4d4dfcbf67122dc4745a81bebb17c6bf6
Theo Sau <t.sautory15@ejm.org>
2022-07-27 23:37:21 -0700

    First running train!

--- loss_functions.py
+++ loss_functions.py
@@ -1,4 +1,4 @@
-import torch
+import torch, pdb
 
 
 class PDELoss():
@@ -8,14 +8,14 @@ class PDELoss():
         self.mu = mu
 
     def compute_loss(self, x, y, upred, vpred, ppred):
-        dudx, dudy = torch.autograd.grad(upred, [x, y], grad_outputs=torch.ones(upred[:, 0].shape), retain_graph=True, create_graph=True)
-        dvdx, dvdy = torch.autograd.grad(vpred, [x, y], grad_outputs=torch.ones(vpred[:, 1].shape), retain_graph=True, create_graph=True)
-        dpdx, dpdy = torch.autograd.grad(ppred, [x, y], grad_outputs=torch.ones(ppred[:, 2].shape), retain_graph=True)
+        dudx, dudy = torch.autograd.grad(upred, [x, y], grad_outputs=torch.ones(upred.shape), retain_graph=True, create_graph=True)
+        dvdx, dvdy = torch.autograd.grad(vpred, [x, y], grad_outputs=torch.ones(vpred.shape), retain_graph=True, create_graph=True)
+        dpdx, dpdy = torch.autograd.grad(ppred, [x, y], grad_outputs=torch.ones(ppred.shape), retain_graph=True)
 
-        d2udx2 = torch.autograd.grad(dudx.squeeze(), x, grad_outputs=torch.ones(dudx[:, 0].shape), retain_graph=True)[0]
-        d2udy2 = torch.autograd.grad(dudy.squeeze(), y, grad_outputs=torch.ones(dudy[:, 0].shape), retain_graph=True)[0]
-        d2vdx2 = torch.autograd.grad(dvdx.squeeze(), x, grad_outputs=torch.ones(dvdx[:, 1].shape), retain_graph=True)[0]
-        d2vdy2 = torch.autograd.grad(dvdy.squeeze(), y, grad_outputs=torch.ones(dvdy[:, 1].shape), retain_graph=True)[0]
+        d2udx2 = torch.autograd.grad(dudx.squeeze(), x, grad_outputs=torch.ones(dudx.squeeze().shape), retain_graph=True)[0]
+        d2udy2 = torch.autograd.grad(dudy.squeeze(), y, grad_outputs=torch.ones(dudy.squeeze().shape), retain_graph=True)[0]
+        d2vdx2 = torch.autograd.grad(dvdx.squeeze(), x, grad_outputs=torch.ones(dvdx.squeeze().shape), retain_graph=True)[0]
+        d2vdy2 = torch.autograd.grad(dvdy.squeeze(), y, grad_outputs=torch.ones(dvdy.squeeze().shape), retain_graph=True)[0]
 
         continuity_error = torch.sum((dudx.squeeze() + dvdy.squeeze())**2)
         xmomentum_error = torch.sum((self.rho*(upred*dudx.squeeze() + vpred*dudy.squeeze()) + dpdx.squeeze() - self.mu*(d2udx2.squeeze() + d2udy2.squeeze()))**2)
--- train_script.py
+++ train_script.py
@@ -1,11 +1,14 @@
-import torch, tqdm
+import torch, pdb
+from tqdm import tqdm
 from cnn_models import ConvAE
 from loss_functions import PDELoss
 from torch.utils.data import DataLoader, Dataset
 
 class ChannelFLow(Dataset):
-    def __init__(self, data):
+    def __init__(self, x, y, data):
         super(ChannelFLow, self).__init__()
+        self.x = x
+        self.y = y
         self.data = data
         return
     
@@ -13,8 +16,10 @@ class ChannelFLow(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
+        self.x = x[idx]
+        self.y = y[idx]
         self.slice = self.data[idx]
-        return self.slice
+        return self.x, self.y, self.slice
 
 if __name__ == "__main__":
 
@@ -24,39 +29,43 @@ if __name__ == "__main__":
 
     # image size
     input_size = 320
+    num_samples = 32
 
     # beta loss weight parameter
-    beta = 0.5
+    beta = 0.
 
     # epochs
     num_epochs = 10
 
     # willl have to load the data
-    x = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    y = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    u = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    v = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    p = torch.randn((10, input_size, input_size, 1), requires_grad=True)
+    print('Generating random data')
+    x = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True).permute((0, 3, 1, 2))
+    y = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True).permute((0, 3, 1, 2))
+    u = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True)
+    v = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True)
+    p = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True)
 
-    x_val = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    y_val = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    u_val = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    v_val = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-    p_val = torch.randn((10, input_size, input_size, 1), requires_grad=True)
-
+    x_val = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True).permute((0, 3, 1, 2))
+    y_val = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True).permute((0, 3, 1, 2))
+    u_val = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True)
+    v_val = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True)
+    p_val = torch.randn((num_samples, input_size, input_size, 1), requires_grad=True)
+    print('Generated data')
+    
     # data
-    train_data = torch.cat([x, y, u, v, p], axis=-1)
+    train_data = torch.cat([u, v, p], axis=-1)
     train_data = train_data.permute((0, 3, 1, 2))
-    val_data = torch.cat([x_val, y_val, u_val, v_val, p_val], axis=-1)
+    val_data = torch.cat([u_val, v_val, p_val], axis=-1)
     val_data = val_data.permute((0, 3, 1, 2)) 
 
+    # pdb.set_trace()
     # datasets
-    train_dataset = ChannelFLow(train_data)
-    val_dataset = ChannelFLow(val_data)
+    train_dataset = ChannelFLow(x, y, train_data)
+    val_dataset = ChannelFLow(x_val, y_val, val_data)
 
     # dataloaders
-    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=False)
-    val_dataloader = DataLoader(val_dataset, batch_size=128, shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=False)
+    val_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=False)
 
     # setup model
     model = ConvAE(input_size=input_size)
@@ -67,17 +76,21 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     ae_loss_function = torch.nn.MSELoss(reduction='mean')
     pde_loss_function = PDELoss()
-            
+
+    print('Setup the model, dataloader, datasets, loss funcitons, optimizers.')     
     for epoch in tqdm(range(num_epochs)):
-        for i, slice in tqdm(enumerate(train_dataloader)):
-            slice = slice.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+        for i, (x_sample, y_sample, slice) in tqdm(enumerate(train_dataloader)):
+            x_sample = x_sample.to(device=device, dtype=dtype)
+            y_sample = y_sample.to(device=device, dtype=dtype)
+            slice = slice.to(device=device, dtype=dtype) #.requires_grad_(True)  # move to device, e.g. GPU
             # ===================forward=====================
-            reconstruction = model.forward(slice)
+            reconstruction = model.forward(torch.cat([x_sample, y_sample, slice], axis=1))
             # =====================loss======================
-            ae_loss = ae_loss_function(slice[:, 2:], reconstruction)
-            pde_loss = pde_loss_function(slice[:, 0], slice[:, 1], slice[:, 2], slice[:, 3], slice[:, 4])
+            pde_loss = pde_loss_function.compute_loss(x_sample, y_sample, reconstruction[:, 0], reconstruction[:, 1], reconstruction[:, 2])
+            ae_loss = ae_loss_function(slice, reconstruction)
             loss = beta * ae_loss + (1-beta) * pde_loss
             # ===================backward====================
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            print(loss)
