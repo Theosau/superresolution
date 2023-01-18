72efcef8d62f29aa860430e19a9a53ffa2c56d6a
Theo Sau <t.sautory15@ejm.org>
2023-01-18 15:00:45 -0800

    web app predicting, zoom functionality is on

--- webApp/app.py
+++ webApp/app.py
@@ -189,27 +189,39 @@ from src.components import ids
 from dash.dependencies import Input, Output, State
 # import os
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=['https://fonts.googleapis.com/css?family=Lato'])
 
 app.layout = html.Div(children=[
-    html.H1(children='PC-MRI flow denoising and super-resolution'),
+    html.H1(children='PC-MRI flow denoising and super-resolution', style={'textAlign': 'center', 'font-family': 'Lato'}),
     dcc.Dropdown(
         id=ids.FILE_DROPDOWN,
         options=[
             {'label': f, 'value': f} for f in os.listdir('src/data/')
         ],
+        style={'font-family': 'Lato'},
         placeholder='Select a file',
         value=None
     ),
     dcc.Store(id=ids.FILE_DROPDOWN_STORE, data=None),
     dcc.Graph(id=ids.IMAGE_PLOT_1, style={'display': 'inline-block'}),
     dcc.Graph(id=ids.IMAGE_PLOT_2, style={'display': 'inline-block'}),
-    html.Button('Denoise', id=ids.UPDATE_FIGURE_2, n_clicks=0),
+    html.Button(
+        children=['Denoise'], 
+        id=ids.UPDATE_FIGURE_2, 
+        n_clicks=0, 
+        style={
+            'font-family': 'Lato',
+            # 'background-color': 'blue',
+            # 'color': 'white',
+            # 'font-size': '18px',
+            # 'padding': '10px',
+            # 'border-radius': '5px',
+        }),
     html.Div(),
-    dcc.Textarea(id=ids.COORDINATES, value="Coordinates: x=[0; 64], [y=0; 64]", style={'width': '50%'}),
+    dcc.Textarea(id=ids.COORDINATES, value="Coordinates: x=[0; 64], [y=0; 64]", style={'width': '50%', 'font-family': 'Lato'}),
     dcc.Store(id=ids.RELAYOUT_DATA_STATE, data=None),
     html.Div(),
-    html.Button('Super-resolve', id=ids.SUPERRESOLVE_FIGURE_1, n_clicks=0),
+    html.Button(children=['Super-resolve'], id=ids.SUPERRESOLVE_FIGURE_1, n_clicks=0, style={'font-family': 'Lato'}),
 ])
 
 @app.callback(Output(ids.IMAGE_PLOT_1, 'figure'), Input(ids.FILE_DROPDOWN, 'value'))
@@ -219,7 +231,7 @@ def update_output(content):
         path = 'src/data/' + content
         array = np.load(path, allow_pickle=True)
         # print(array.argmax(axis=2))
-        sliced = array[0, 0, 15, ...]
+        sliced = array[0, 0, ..., 15]
         fig1 = px.imshow(sliced, title="Input image")
         return fig1
     else:
@@ -310,19 +322,90 @@ def denoise_input(n_clicks_denoise, n_clicks_superresolve, value, data, relayout
             predictions[k] = a.reshape((4, 64, 64, 64))
         
 
-        model_output = predictions.detach().cpu().numpy()[0, 0, 15, ...]
+        model_output = predictions.detach().cpu().numpy()[0, 0, ..., 15]
         fig1 = px.imshow(model_output, title="Model Output")
 
     elif ctx.triggered[0]['prop_id'] == f'{ids.SUPERRESOLVE_FIGURE_1}.n_clicks':
-        array = np.zeros(shape=(2,2))
-        fig1 = px.imshow(array, title="Model Output")
+        # array = np.zeros(shape=(2,2))
+        # fig1 = px.imshow(array, title="Model Output")
 
         x_coord_l = relayout_data['xaxis.range[0]']
         x_coord_r = relayout_data['xaxis.range[1]']
-        y_coord_l = relayout_data['yaxis.range[0]']
-        y_coord_r = relayout_data['yaxis.range[1]']
+        # y_coord_l = relayout_data['yaxis.range[0]']
+        # y_coord_r = relayout_data['yaxis.range[1]']
+        y_coord_b = relayout_data['yaxis.range[0]']
+        y_coord_t = relayout_data['yaxis.range[1]']
 
+        path = 'src/data/' + content
+        array = torch.from_numpy(np.load(path, allow_pickle=True))[:1, ...]
+        path_seg = path.split('.')[0] + '_segmentation.npy'
+        segmentation = torch.from_numpy(np.load(path_seg, allow_pickle=True))[:1, ...]
+
+        predictions = torch.from_numpy(np.zeros((len(array), 4, array.shape[2], array.shape[3], array.shape[4])))
+        predictions = predictions.float()
+
+        flow_sample = array.to(device=device, dtype=dtype) # move to device, e.g. GPU
+        map_sample = segmentation.to(device=device, dtype=dtype)
+
+        # =====================forward======================
+        # compute latent vectors
+        latent_vectors = model(flow_sample)
         
+        # select same number of points per image to sample, unsqueeze at dim 1 to get the shape
+        # batch_size x 1 x num_points x coordinates_size
+        pts_ints = torch.cat(
+            [get_all_points(map_one.squeeze()).unsqueeze(0) for map_one in map_sample], 
+            dim=0
+        ).unsqueeze(1).unsqueeze(1)
+        
+        # pts_ints = torch.Tensor(np.array([get_all_points(map_one.squeeze()) for map_one in map_sample])).unsqueeze(1).unsqueeze(1)
+        # normalize the points to be in -1, 1 (required by grid_sample)
+        # to have it zoomed, I divide these points here by whatever number
+        # pts = (2*(pts_ints/(array.shape[3]-1)) - 1)/1
+        # ws_small = torch.tensor([x_coord_l, y_coord_r, 0])
+        # ws_large = torch.tensor([x_coord_r, y_coord_l, 63])
+        ws_small = torch.tensor([y_coord_t, x_coord_l, 0])
+        ws_large = torch.tensor([y_coord_b, x_coord_r, 63])
+        ws_small.unsqueeze(0).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+        ws_large.unsqueeze(0).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+        pts = ( (ws_large - ws_small) * (pts_ints/(array.shape[3]-1)) + ws_small)
+        pts = (2*(pts/(array.shape[3]-1)) - 1)
+        
+        # features, segmentation maps, flow values
+        pts_vectors, pts_maps, pts_flows = pp3d(latent_vectors, map_sample, flow_sample, pts)
+        pts_locations = pts.squeeze()
+        pts_locations = pts_locations.unsqueeze(0)
+        pts_locations.requires_grad = True # needed for the PDE loss
+        
+        # create feature vectors for each sampled point
+        feature_vector = torch.cat([pts_locations, pts_maps, pts_vectors], dim=-1) 
+        feature_vector = feature_vector.reshape((-1, model.channels_out + 4)) # x, y, z, seg inter, features
+        
+        # split input features to allow taking separate derivatives
+        inputs = [feature_vector[..., i:i+1] for i in range(feature_vector.shape[-1])]
+        x_ = torch.cat(inputs, axis=-1)
+        
+        # forward through linear model
+        outputs_linear = smallLinear(x_)
+        
+        # points as integers for indexing
+        print(pts_ints.shape)
+        pts_ints_l = pts_ints.squeeze().long()
+        pts_ints_l = pts_ints_l.unsqueeze(0)
+        print(pts_ints_l.shape)
+
+        for k in tqdm(range(len(pts_ints_l))):
+            pts_one_long = pts_ints_l[k]
+            outputs_linear_one = outputs_linear.reshape(array.shape[0], 64**3, 4)[k]
+
+            flat_one_points = ravel_multi_index(pts_one_long, (64, 64, 64))
+            a = torch.zeros((4, 64*64*64), dtype=outputs_linear_one.dtype, requires_grad=False)
+            a[:, flat_one_points] = outputs_linear_one.T
+            predictions[k] = a.reshape((4, 64, 64, 64))
+        
+
+        model_output = predictions.detach().cpu().numpy()[0, 0, ..., 15]
+        fig1 = px.imshow(model_output, title="Model Output")
 
     return fig1
 
@@ -335,14 +418,15 @@ def denoise_input(n_clicks_denoise, n_clicks_superresolve, value, data, relayout
                 State(ids.RELAYOUT_DATA_STATE,'data')
               ])
 def store_coordinates(relayout_data, relayout_data_state):
-    if relayout_data == relayout_data_state or relayout_data == {'autosize': True}:
-        return "Coordinates: x=[0; 64], [y=0; 64]"
+    if relayout_data == relayout_data_state or relayout_data == {'autosize': True} or relayout_data == {'xaxis.autorange': True, 'yaxis.autorange': True}:
+        return "Coordinates: Vertical=[0; 64], Horizontal=[0; 64]"
     else:
+        print(relayout_data)
         x_coord_l = relayout_data['xaxis.range[0]']
         x_coord_r = relayout_data['xaxis.range[1]']
-        y_coord_l = relayout_data['yaxis.range[0]']
-        y_coord_r = relayout_data['yaxis.range[1]']
-        return f"Coordinates: x=[{x_coord_l:.2f}; {x_coord_r:.2f}], [y={y_coord_l:.2f}; {y_coord_r:.2f}]"
+        y_coord_b = relayout_data['yaxis.range[0]']
+        y_coord_t = relayout_data['yaxis.range[1]']
+        return f"Coordinates: Vetical=[{y_coord_t:.2f}; {y_coord_b:.2f}] Horizontal=[{x_coord_l:.2f}; {x_coord_r:.2f}]"
 
 
 # @app.callback(
