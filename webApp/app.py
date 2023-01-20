0e24e1038d2213b9a709f5b44cddb0afa1765130
Theo Sau <t.sautory15@ejm.org>
2023-01-20 11:00:43 -0800

    update with noise

--- webApp/app.py
+++ webApp/app.py
@@ -61,16 +61,6 @@ background_pde = 1 - background_recon
 recon_loss_function = ReconLoss()
 pde_loss_function = PDELoss(rho=config["rho"], mu=config["mu"], gx=config["gx"], gy=config["gy"], gz=config["gz"])
 
-# volume size
-# nvox = config["nvox"]
-# nsamples = 100 #config["nsamples"]
-# samples, segmentation_maps = generate_dataset(nsamples=nsamples, nvox=nvox)
-# np.save('poiseuille_volume.npy', samples)
-# np.save('poiseuille_volume_segmentation.npy', segmentation_maps)
-# val_dataset = PoseuilleFlowAnalytic(samples, segmentation_maps)
-# batch_size = 1 #config["batch_size"]
-# val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
 # setup models
 model = ConvUNetBis(
     input_size=config["nvox"], 
@@ -86,96 +76,15 @@ smallLinear = SmallLinear(num_features=model.channels_out+4, num_outputs=4) # + 
 smallLinear.eval()
 print("There are ", sum(p.numel() for p in smallLinear.parameters()), " parameters to train.")
 
+model_name = "test_noisy_data"
 checkpoint = torch.load(f"trainings/saved_models/{model_name}", map_location=torch.device("cpu"))
+model_name = "current_model"
 model.load_state_dict(checkpoint["model_state_dict"])
 smallLinear.load_state_dict(checkpoint["linear_model_state_dict"])
 
 # point sampler
 pp3d = PointPooling3D(interpolation="trilinear")
 
-# epoch_total_loss = 0
-# epoch_recon_loss = 0
-# epoch_pde_loss = 0
-
-# predictions = torch.from_numpy(np.zeros((len(samples), 4, samples.shape[2], samples.shape[3], samples.shape[4])))
-# predictions = predictions.float()
-
-# iterate through the dataset
-# for i, (flow_sample, map_sample) in enumerate(val_dataloader):
-#     flow_sample = flow_sample.to(device=device, dtype=dtype) # move to device, e.g. GPU
-#     map_sample = map_sample.to(device=device, dtype=dtype)
-
-#     # =====================forward======================
-#     # compute latent vectors
-#     latent_vectors = model(flow_sample)
-    
-#     # select same number of points per image to sample, unsqueeze at dim 1 to get the shape
-#     # batch_size x 1 x num_points x coordinates_size
-    
-#     pts_ints = torch.cat(
-#         [get_all_points(map_one.squeeze()).unsqueeze(0) for map_one in map_sample], 
-#         dim=0
-#     ).unsqueeze(1).unsqueeze(1)
-    
-# #     pts_ints = torch.Tensor(np.array([get_all_points(map_one.squeeze()) for map_one in map_sample])).unsqueeze(1).unsqueeze(1)
-#     # normalize the points to be in -1, 1 (required by grid_sample)
-#     # to have it zoomed, I divide these points here by whatever number
-#     pts = (2*(pts_ints/(nvox-1)) - 1)/1
-    
-#     # features, segmentation maps, flow values
-#     pts_vectors, pts_maps, pts_flows = pp3d(latent_vectors, map_sample, flow_sample, pts)
-#     pts_locations = pts.squeeze()
-#     pts_locations.requires_grad = True # needed for the PDE loss
-    
-#     # create feature vectors for each sampled point
-#     feature_vector = torch.cat([pts_locations, pts_maps, pts_vectors], dim=-1) 
-#     feature_vector = feature_vector.reshape((-1, model.channels_out + 4)) # x, y, z, seg inter, features
-    
-#     # split input features to allow taking separate derivatives
-#     inputs = [feature_vector[..., i:i+1] for i in range(feature_vector.shape[-1])]
-#     x_ = torch.cat(inputs, axis=-1)
-    
-#     # forward through linear model
-#     outputs_linear = smallLinear(x_)
-    
-#     # points as integers for indexing
-#     pts_ints_l = pts_ints.squeeze().long()
-
-#     for k in tqdm(range(len(pts_ints_l))):
-#         pts_one_long = pts_ints_l[k]
-#         outputs_linear_one = outputs_linear.reshape(batch_size, 64**3, 4)[k]
-
-#         flat_one_points = ravel_multi_index(pts_one_long, (64, 64, 64))
-#         a = torch.zeros((4, 64*64*64), dtype=outputs_linear_one.dtype, requires_grad=False)
-#         a[:, flat_one_points] = outputs_linear_one.T
-#         predictions[i*batch_size + k] = a.reshape((4, 64, 64, 64))
-        
-#     # =====================losses======================
-#     # get the losses weights for each point
-#     num_loss_terms = 2
-#     seg_interpolations_rand = pts_maps.reshape((-1, 1))
-#     weights = torch.ones((len(seg_interpolations_rand), num_loss_terms), dtype=torch.float)
-#     # boundary weights
-#     weights[(seg_interpolations_rand>=0.75).squeeze(), :] = torch.Tensor([boundary_pde, boundary_recon])
-#     # flow weights
-#     weights[(seg_interpolations_rand>=1.25).squeeze(), :] = torch.Tensor([flow_pde, flow_recon])
-#     # background weights
-#     weights[(seg_interpolations_rand<0.5).squeeze(), :] = torch.Tensor([background_pde, background_recon])
-    
-#     # compute the loss
-#     pde_loss = weights[:, 0]*pde_loss_function.compute_loss(inputs, outputs_linear)
-#     recon_loss = weights[:, 1]*recon_loss_function.compute_loss(pts_flows, outputs_linear)
-#     loss = torch.mean(pde_loss + recon_loss)
-    
-#     # update write iteration loss
-#     epoch_total_loss += loss.item()
-#     epoch_recon_loss += recon_loss.mean().item()
-#     epoch_pde_loss += pde_loss.mean().item()
-
-# print("Total loss: ", epoch_total_loss, "Reconstruction loss: ", epoch_recon_loss, "PDE loss: ", epoch_pde_loss)
-# # np.save("predictions.npy", predictions.detach().cpu().numpy())
-
-# predictions_np = predictions.detach().cpu().numpy()
 
 os.chdir(initial_dir)
 ##########################################################################
@@ -230,8 +139,27 @@ def update_output(content):
         # return f'You selected file: {content}'
         path = 'src/data/' + content
         array = np.load(path, allow_pickle=True)
-        # print(array.argmax(axis=2))
+        path_seg = path.split('.')[0] + '_segmentation.npy'
+        segmentation = np.load(path_seg, allow_pickle=True)
+
         sliced = array[0, 0, ..., 15]
+        seg_sliced = segmentation[0, 0, ..., 15]
+        ######################################################################
+        # adding noise here to see if its just identity :P
+        non_centrality = 0.1
+        df = 2
+        noise = np.clip(
+            np.random.noncentral_chisquare(
+                df, 
+                non_centrality, 
+                size = [sliced.shape[0], sliced.shape[1]]), 
+            np.min(sliced), 
+            np.max(sliced)
+        )
+        noise[seg_sliced==0]=0
+        sliced += noise
+        ######################################################################
+
         fig1 = px.imshow(sliced, title="Input image")
         return fig1
     else:
@@ -263,9 +191,30 @@ def denoise_input(n_clicks_denoise, n_clicks_superresolve, value, data, relayout
 
     elif ctx.triggered[0]['prop_id'] == f'{ids.UPDATE_FIGURE_2}.n_clicks':
         path = 'src/data/' + content
-        array = torch.from_numpy(np.load(path, allow_pickle=True))[:1, ...]
+        array = np.load(path, allow_pickle=True)
         path_seg = path.split('.')[0] + '_segmentation.npy'
-        segmentation = torch.from_numpy(np.load(path_seg, allow_pickle=True))[:1, ...]
+        segmentation = np.load(path_seg, allow_pickle=True)
+        ######################################################################
+        # adding noise here to see if its just identity :P
+        non_centrality = 0.1
+        df = 2
+        noise = np.clip(
+            np.random.noncentral_chisquare(
+                df, 
+                non_centrality, 
+                size = [1, array.shape[1], array.shape[2], array.shape[3], array.shape[4]]), 
+            np.min(array[:1, ...], axis=0), 
+            np.max(array[:1, ...], axis=0)
+        )
+        segmentation_maps_rep = np.repeat(segmentation[:1, ...], 3, axis=1)
+        noise[segmentation_maps_rep==0]=0
+        array += noise
+        ######################################################################
+        array = torch.from_numpy(array)[:1, ...]
+        segmentation = torch.from_numpy(segmentation)[:1, ...]
+
+        # path_seg = path.split('.')[0] + '_segmentation.npy'
+        # segmentation = torch.from_numpy(np.load(path_seg, allow_pickle=True))[:1, ...]
 
         predictions = torch.from_numpy(np.zeros((len(array), 4, array.shape[2], array.shape[3], array.shape[4])))
         predictions = predictions.float()
@@ -307,10 +256,8 @@ def denoise_input(n_clicks_denoise, n_clicks_superresolve, value, data, relayout
         outputs_linear = smallLinear(x_)
         
         # points as integers for indexing
-        print(pts_ints.shape)
         pts_ints_l = pts_ints.squeeze().long()
         pts_ints_l = pts_ints_l.unsqueeze(0)
-        print(pts_ints_l.shape)
 
         for k in tqdm(range(len(pts_ints_l))):
             pts_one_long = pts_ints_l[k]
@@ -326,20 +273,34 @@ def denoise_input(n_clicks_denoise, n_clicks_superresolve, value, data, relayout
         fig1 = px.imshow(model_output, title="Model Output")
 
     elif ctx.triggered[0]['prop_id'] == f'{ids.SUPERRESOLVE_FIGURE_1}.n_clicks':
-        # array = np.zeros(shape=(2,2))
-        # fig1 = px.imshow(array, title="Model Output")
-
         x_coord_l = relayout_data['xaxis.range[0]']
         x_coord_r = relayout_data['xaxis.range[1]']
-        # y_coord_l = relayout_data['yaxis.range[0]']
-        # y_coord_r = relayout_data['yaxis.range[1]']
         y_coord_b = relayout_data['yaxis.range[0]']
         y_coord_t = relayout_data['yaxis.range[1]']
 
         path = 'src/data/' + content
-        array = torch.from_numpy(np.load(path, allow_pickle=True))[:1, ...]
+        # array = torch.from_numpy(np.load(path, allow_pickle=True))[:1, ...]
+        array = np.load(path, allow_pickle=True)
         path_seg = path.split('.')[0] + '_segmentation.npy'
-        segmentation = torch.from_numpy(np.load(path_seg, allow_pickle=True))[:1, ...]
+        segmentation = np.load(path_seg, allow_pickle=True)
+        ######################################################################
+        # adding noise here to see if its just identity :P
+        non_centrality = 0.1
+        df = 2
+        noise = np.clip(
+            np.random.noncentral_chisquare(
+                df, 
+                non_centrality, 
+                size = [1, array.shape[1], array.shape[2], array.shape[3], array.shape[4]]), 
+            np.min(array[:1, ...], axis=0), 
+            np.max(array[:1, ...], axis=0)
+        )
+        segmentation_maps_rep = np.repeat(segmentation[:1, ...], 3, axis=1)
+        noise[segmentation_maps_rep==0]=0
+        array += noise
+        ######################################################################
+        array = torch.from_numpy(array)[:1, ...]
+        segmentation = torch.from_numpy(segmentation)[:1, ...]
 
         predictions = torch.from_numpy(np.zeros((len(array), 4, array.shape[2], array.shape[3], array.shape[4])))
         predictions = predictions.float()
@@ -360,10 +321,6 @@ def denoise_input(n_clicks_denoise, n_clicks_superresolve, value, data, relayout
         
         # pts_ints = torch.Tensor(np.array([get_all_points(map_one.squeeze()) for map_one in map_sample])).unsqueeze(1).unsqueeze(1)
         # normalize the points to be in -1, 1 (required by grid_sample)
-        # to have it zoomed, I divide these points here by whatever number
-        # pts = (2*(pts_ints/(array.shape[3]-1)) - 1)/1
-        # ws_small = torch.tensor([x_coord_l, y_coord_r, 0])
-        # ws_large = torch.tensor([x_coord_r, y_coord_l, 63])
         ws_small = torch.tensor([y_coord_t, x_coord_l, 0])
         ws_large = torch.tensor([y_coord_b, x_coord_r, 63])
         ws_small.unsqueeze(0).unsqueeze(0).unsqueeze(0).unsqueeze(0)
